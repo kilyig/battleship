8bcd87d359188283e83e2c9788064142d7f94fb3
kilyig <yigit@kilicoglu.nom.tr>
2022-08-20 10:06:04 -0400

    work on forceToPlay

--- contracts/BattleshipGame.sol
+++ contracts/BattleshipGame.sol
@@ -1,39 +1,137 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+
 import "./interfaces/IBoardsetupVerifier.sol";
 import "./interfaces/IFireshotVerifier.sol";
 import "./interfaces/IBattleshipGame.sol";
 
 contract BattleshipGame is IBattleshipGame {
-    
+    using ECDSA for bytes32;
+    using Counters for Counters.Counter;
+
+    uint256 public constant WIN_HIT_COUNT = 5 + 4 + 3 + 3 + 2;
+
     IBoardsetupVerifier boardsetupVerifier;
     IFireshotVerifier fireshotVerifier;
 
-    struct BattleshipGameState {
-        /// The address of the two players.
-        address[2] participants;
-        /// The hash committing to the ship configuration of each playment.
-        uint256[2] boards;
-        /// The turn number of this game.
-        uint256 turn;
-        /// Mapping of player shot indices to turn number.
-        /// The shot indices of the second player are offset by 100.
-        mapping(uint256 => uint256) shots;
-        /// The number of hits each player has made on a ship.
-        uint256[2] hits;
-        /// The winner of the game.
-        address winner;
-    }
+    Counters.Counter private _ticketCounter;
+    mapping(uint256 => uint256) private shots;
 
     constructor(address _boardsetupVerifier, address _fireshotVerifier) {
         boardsetupVerifier = IBoardsetupVerifier(_boardsetupVerifier);
         fireshotVerifier = IFireshotVerifier(_fireshotVerifier);
     }
 
+    mapping (uint256 => BattleshipGameState) tickets;
+
+    modifier isPlayer(address supposedPlayer, BattleshipGameState memory gameState) {
+        require(
+            supposedPlayer == gameState.players[0] ||
+            supposedPlayer == gameState.players[1]
+        );
+        _;
+    }
+
+    modifier playsNext(address player, BattleshipGameState memory gameState) {
+        require(
+            player == gameState.players[nextPlayerIndex(gameState)]
+        );
+        _;
+    }
+
+    function nextPlayerIndex(BattleshipGameState memory gameState) internal pure returns (uint256) {
+        return gameState.turn % 2;
+    }
+
+    function verifyGameState(
+        BattleshipGameState memory gameState,
+        address desiredSigner,
+        bytes memory signature
+        ) internal pure returns (bool) {
+        // Hash those params!
+        bytes32 messagehash =  keccak256(abi.encode(gameState));
+        // See who's the signer
+        address signer = messagehash.toEthSignedMessageHash().recover(signature);
+        if (desiredSigner  == signer) {
+            // Checks out!
+            return (true);
+        } else {
+            // Looks bogus.
+            return (false);
+        }
+    }
+
+    function iterateGameState(
+        BattleshipGameState memory _gameState,
+        uint256 _hitShipId
+    ) internal pure {
+        uint256 playerThatShotIndex = nextPlayerIndex(_gameState);
+        if(_hitShipId != 0) {
+            _gameState.hits[playerThatShotIndex] += 1;
+            if(_gameState.hits[playerThatShotIndex] == WIN_HIT_COUNT) {
+                _gameState.winner = _gameState.players[playerThatShotIndex];
+            }
+        }
+
+        _gameState.turn += 1;
+    }
+
+    function isFinished(BattleshipGameState memory _gameState) public pure returns (bool) {
+        return _gameState.winner == _gameState.players[0] ||
+               _gameState.winner == _gameState.players[1];
+    }
+
     function forceToPlay(
-        uint256 _game, uint256[2] memory _shot
+        BattleshipGameState memory _gameStateLastAgreed,
+        bytes memory forceeSignatureLastAgreed,
+        FireshotResultData memory _fireshotResultData,
+        uint256[2] memory _shot
+    ) external playsNext(msg.sender, _gameStateLastAgreed) {
+        // verify that the forcee had really agreed on the state
+        address forceeAddress = _gameStateLastAgreed.players[(nextPlayerIndex(_gameStateLastAgreed) + 1) % 2];
+        require(verifyGameState(_gameStateLastAgreed,
+                                forceeAddress,
+                                forceeSignatureLastAgreed));
+
+        // check that the forcer has called hit/miss correctly
+        // the first shot of the game is an exception as the starter
+        // only fires a shot and doesn't need to call
+        if(_gameStateLastAgreed.turn != 0) {
+            require(fireshotVerifier.verifyProof(_fireshotResultData.a,
+                                                 _fireshotResultData.b,
+                                                 _fireshotResultData.c,
+                                                 [_fireshotResultData._hitShipId,
+                                                  _gameStateLastAgreed.boardHashes[nextPlayerIndex(_gameStateLastAgreed)],
+                                                  _fireshotResultData._prevTurnShotIndex]),
+                                                 "Invalid proof!");
+        }
+
+        // update the game state with the result of the forcee's shot
+        iterateGameState(_gameStateLastAgreed, _fireshotResultData._hitShipId);
+
+        // the forcee needs to make the next move only if the forcer's move
+        // did not already finish the game.
+        bool forceeNeedsToReply = !isFinished(_gameStateLastAgreed);
+        if(forceeNeedsToReply) {
+            uint256 forceeDeadline = block.timestamp + _gameStateLastAgreed.forceePlayInterval;  
+        }
+
+
+
+
+
+
+    }
+
+    function play(
+        BattleshipGameState memory _gameStateAfterForcedTurn,
+        bytes memory forceeForcedTurnSignature,
+        uint256[2] memory _shot
     ) external {
 
     }
+
 }
--- contracts/interfaces/IBattleshipGame.sol
+++ contracts/interfaces/IBattleshipGame.sol
@@ -12,5 +12,94 @@ interface IBattleshipGame {
     // event ShotLanded(uint256 _gameId, uint8 _shipId);
     // event Won(uint256 _gameId, address _by);
 
+    struct BattleshipGameState {
+        // identifier for the games between the two players.
+        // (not a unique identifier for all the games played by all players)
+        uint256 gameId;
+        // The address of the two players.
+        address[2] players;
+        // The hash committing to the ship configuration of each game.
+        uint256[2] boardHashes;
+        // The turn number of this game.
+        uint256 turn;
+        // Mapping of player shot indices to turn number.
+        // The shot indices of the second player are offset by 100.
+        //mapping(uint256 => uint256) shots;
+        // The number of hits each player has made on a ship.
+        uint256[2] hits;
+        // The winner of the game.
+        address winner;
+
+        uint256 forceePlayInterval;
+    }
+
+    struct FireshotResultData {
+        uint256 _hitShipId;
+        uint256 _prevTurnShotIndex;
+        uint256 _turnShotIndex;
+        uint256 deneme;
+        uint256[2] a;
+        uint256[2][2] b;
+        uint256[2] c;
+    }
+
+    function forceToPlay(
+        BattleshipGameState memory _gameStateLastAgreed,
+        bytes memory forceeSignatureLastAgreed,
+        FireshotResultData memory _fireshotResultData,
+        uint256[2] memory _shot
+    ) external;
+
+    function play(
+        BattleshipGameState memory _gameStateAfterForcedTurn,
+        bytes memory forceeForcedTurnSignature,
+        uint256[2] memory _shot
+    ) external;
+
+
+    /*
+     * @dev modifier isPlayer
+     * Called by a player when they feel a need to force the other player
+     * to share the result of the latest shot (hit/miss). Such a situation
+     * can arise if a losing player is intentionally slowing down the game
+     * or sending invalid messages. This function declares the timestamp before
+     * which the other player must have submitted the result of the shot by.
+     * The function calculates the timestamp from the game state.
+     *
+     * @param _lastAgreedGameState uint256 - hash of ship placement on board
+     * @param a uint256[2] - zk proof part 1
+     * @param b_0 uint256[2] - zk proof part 2 split 1
+     * @param b_1 uint256[2] - zk proof part 2 split 2
+     * @param c uint256[2] - zk proof part 3
+     */
+    // function forceToShareShotResult(
+    //     BattleshipGameState memory _lastAgreedGameState,
+    //     bytes memory otherPlayerSignature,
+    //     uint256[2] memory _shot
+    // ) external;
+
+    /* TODO: correct the descriptions below
+     * Called by a player when they feel a need to force the other player
+     * to share the result of the latest shot (hit/miss). Such a situation
+     * can arise if a losing player is intentionally slowing down the game
+     * or sending invalid messages. This function declares the timestamp before
+     * which the other player must have submitted the result of the shot by.
+     * The function calculates the timestamp from the game state.
+     * @dev modifier isPlaying
+     *
+     * @param _lastAgreedGameState uint256 - hash of ship placement on board
+     * @param a uint256[2] - zk proof part 1
+     * @param b uint256[2][] - zk proof part 2
+     * @param c uint256[2] - zk proof part 3
+     */
+    // function shareShotResult(
+    //     BattleshipGameState memory _gameStateAfterShotResult,
+    //     bytes memory signature,
+    //     uint256 _hitShipId,
+    //     uint256[2] memory a,
+    //     uint256[2][2] memory b,
+    //     uint256[2] memory c
+    // ) external;
+
 
 }
